bdaeb392cc74f64e2a56b7d65d97a21a04ff4576
DannyAtal <95615130+DannyAtal@users.noreply.github.com>
2024-01-03 11:08:58 +0200

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -3,3 +3,4 @@ pip install -q -U git+https://github.com/huggingface/accelerate.git
 pip install -q -U bitsandbytes
 pip install -q -U git+https://github.com/huggingface/transformers.git
 pip install sentencepiece
+pip install -U -r qlora/requirements.txt
